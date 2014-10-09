a9a9e82c099f84858d1ac42bd0577d250d5079c2
Andrew Halberstadt <halbersa@gmail.com>
2014-10-09 15:21:17 -0400

    Setup initial structure

---
+++ setup.py
@@ -0,0 +1,37 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from setuptools import setup, find_packages
+
+PACKAGE_VERSION = '0.1'
+
+deps = [
+    'manifestparser',
+    'marionette-client',
+]
+
+setup(name='marionette-release-tests',
+      version=PACKAGE_VERSION,
+      description='A collection of release tests for Mozilla Firefox',
+      long_description='See https://github.com/ahal/marionette-release-tests',
+      classifiers=['Environment :: Console',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
+                   'Natural Language :: English',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Software Development :: Libraries :: Python Modules',
+                   ],
+      keywords='mozilla',
+      author='Andrew Halberstadt',
+      author_email='ahalberstadt@mozilla.com',
+      url='https://github.com/ahal/marionette-release-tests',
+      license='MPL 2.0',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=deps,
+      entry_points="""
+        run-release-tests = harness.runtests:run
+      """)
