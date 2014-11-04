75beb99600edb9c878f3723dc96e924948f906f7
Henrik Skupin <mail@hskupin.info>
2014-11-04 17:22:04 +0100

    Fix broken test for l10n module

--- greenlight/lib/tests/test_l10n.py
+++ greenlight/lib/tests/test_l10n.py
@@ -4,28 +4,33 @@
 
 from marionette.errors import MarionetteException
 
+from greenlight.harness.decorators import uses_lib
 from greenlight.harness.testcase import FirefoxTestCase
 
 
 class TestL10n(FirefoxTestCase):
 
+    @uses_lib('l10n')
     def test_dtd_entity(self):
         dtds = ['chrome://global/locale/filepicker.dtd',
-                'chrome://branding/locale/brand.dtd']
+                'chrome://browser/locale/baseMenuOverlay.dtd']
 
-        value = self.l10n.get_localized_entity(dtds, 'vendorShortName')
-        self.assertEqual(value, 'Mozilla')
+        value = self.l10n.get_localized_entity(dtds, 'helpSafeMode.label')
+        elm = self.marionette.find_element('id', 'helpSafeMode')
+        self.assertEqual(value, elm.get_attribute('label'))
 
         self.assertRaises(MarionetteException,
                           self.l10n.get_localized_entity,
                           dtds, 'notExistent')
 
+    @uses_lib('l10n')
     def test_properties(self):
         properties = ['chrome://global/locale/filepicker.properties',
-                      'chrome://branding/locale/brand.properties']
+                      'chrome://global/locale/findbar.properties']
 
-        value = self.l10n.get_localized_property(properties, 'vendorShortName')
-        self.assertEqual(value, 'Mozilla')
+        # TODO: Find a way to verify the retrieved translated string
+        value = self.l10n.get_localized_property(properties, 'NotFound')
+        self.assertNotEqual(value, '')
 
         self.assertRaises(MarionetteException,
                           self.l10n.get_localized_property,
