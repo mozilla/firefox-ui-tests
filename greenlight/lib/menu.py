ecae2ea58ca56bfbfb545ffc66afe1461f656d8d
Andrew Halberstadt <halbersa@gmail.com>
2014-11-11 16:44:23 -0500

    Add library for working with the menubar

---
+++ greenlight/lib/menu.py
@@ -0,0 +1,118 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from marionette import NoSuchElementException
+
+from . import (
+    DOMElement,
+    using_context
+)
+
+
+class MenuPanel(object):
+    def __init__(self, client):
+        self.client = client
+
+    @property
+    def popup(self):
+        return self.MenuPanelElement.create(self.client.find_element('id', 'PanelUI-popup'))
+
+
+    class MenuPanelElement(DOMElement):
+        _buttons = None
+
+        @property
+        def buttons(self):
+            if not self._buttons:
+                self._buttons = self.find_element('id', 'PanelUI-multiView') \
+                                    .find_element('anon attribute', {'anonid': 'viewContainer'}) \
+                                    .find_elements('tag name', 'toolbarbutton')
+            return self._buttons
+
+        def click(self, target=None):
+            """
+            Overrides HTMLElement.click to provide a target to click.
+
+            :param target: The label associated with the button to click on, e.g 'New Private Window'.
+            :returns: The window id of the preferences window to be used with
+            """
+            if not target:
+                return DOMElement.click(self)
+
+            for button in self.buttons:
+                if button.get_attribute('label') == target:
+                    return button.click()
+            raise NoSuchElementException("Could not find '{}' in the menu panel UI".format(target))
+
+
+class MenuBar(object):
+    """
+    Class for manipulating the Firefox menubar.
+    """
+
+    def __init__(self, client):
+        self.client = client
+
+    @property
+    @using_context('chrome')
+    def menus(self):
+        """
+        :returns: A list of MenuElements corresponding to the top level menus
+                  in the menubar.
+        """
+        menus = self.client.find_element('id', 'main-menubar') \
+                           .find_elements('tag name', 'menu')
+        return [self.MenuElement.create(menu) for menu in menus]
+
+    @using_context('chrome')
+    def get_menu(self, label):
+        """
+        Get a MenuElement corresponding to the specified label.
+
+        :param label: The label of the menu, e.g 'File' or 'View'
+        :returns: A MenuElement
+        """
+        menu = [m for m in self.menus if m.get_attribute('label') == label]
+
+        if not menu:
+            raise NoSuchElementException("Could not find a menu with label '{}'".format(label))
+
+        return menu[0]
+
+    @using_context('chrome')
+    def select(self, label, item):
+        """
+        Select click on an item in a menu.
+
+        :param label: The label of the menu, e.g 'File' or 'View'
+        :param item: The label of the item in the menu, e.g 'New Tab'
+        """
+        return self.get_menu(label).select(item)
+
+
+    class MenuElement(DOMElement):
+        """
+        An class for manipulating a Menu element.
+        """
+
+        @property
+        def items(self):
+            """
+            Returns a list of menuitem elements within the menu.
+            """
+            return self.find_element('tag name', 'menupopup') \
+                       .find_elements('tag name', 'menuitem')
+
+        def select(self, label):
+            """
+            Click on a menuitem within the menu.
+
+            :param label: The label of the menuitem, e.g 'New Tab'
+            """
+            item = [l for l in self.items if l.get_attribute('label') == label]
+
+            if not item:
+                raise NoSuchElementException("Could not find an item labeled '{}' in the '{}' menu".format(label, self.get_attribute('label')))
+
+            return item[0].click()
--- greenlight/lib/tabs.py
+++ greenlight/lib/tabs.py
@@ -2,11 +2,14 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
+from . import using_context
+
 class Tabs(object):
     def __init__(self, client):
         self.client = client
 
     @property
+    @using_context('chrome')
     def active_tab(self):
         # TODO this doesn't work; see bug 1088223
         for tab in self.tabs:
@@ -14,11 +17,13 @@ class Tabs(object):
                 return tab
 
     @property
+    @using_context('chrome')
     def tabs(self):
         return self.client.find_element('id', 'tabbrowser-tabs') \
                           .find_elements('tag name', 'tab')
 
     @property
+    @using_context('chrome')
     def newtab_button(self):
         return self.client.find_element('id', 'tabbrowser-tabs') \
                           .find_element('anon attribute', {'anonid': 'tabs-newtab-button'})
