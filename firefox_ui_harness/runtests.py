16a2a3a8dd49c1e5854b2e7cf506b4876b59fa7e
Barbara Miller <galgeek@me.com>
2015-01-29 15:10:05 -0800

    Bug 1123683 - Set default testing preferences. r=whimboo

--- firefox_ui_harness/runtests.py
+++ firefox_ui_harness/runtests.py
@@ -11,22 +11,19 @@ from marionette.runtests import cli
 import firefox_ui_tests
 
 from .arguments import ReleaseTestParser
+from .default_prefs import default_prefs
 from .testcase import FirefoxTestCase
 
 
 class ReleaseTestRunner(BaseMarionetteTestRunner):
-    extra_prefs = {
-        "browser.tabs.remote.autostart": False,
-    }
-
     def __init__(self, *args, **kwargs):
         if not kwargs.get('server_root'):
             kwargs['server_root'] = firefox_ui_tests.resources
 
         prefs = kwargs.get('prefs', {})
-        extra_prefs = copy.deepcopy(ReleaseTestRunner.extra_prefs)
-        extra_prefs.update(prefs)
-        kwargs['prefs'] = extra_prefs
+        runner_prefs = copy.deepcopy(default_prefs)
+        runner_prefs.update(prefs)
+        kwargs['prefs'] = runner_prefs
 
         BaseMarionetteTestRunner.__init__(self, *args, **kwargs)
         self.test_handlers = [FirefoxTestCase]
