05ff71ed12d73112a8139a6c118dda4696e3e5c5
Barbara Miller <galgeek@me.com>
2015-08-17 17:57:08 -0700

    Bug 1171400 - Increase screen size to 1280x768 for Travis CI runs on Linux. r=hskupin
    (cherry picked from commit 28f7c2c16a3ddacd1a44c6a5b5bb05ab1672bd25)

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -3,7 +3,7 @@ set -ev
 
 if [ "$TRAVIS_OS_NAME" == "linux" ]; then
     /sbin/start-stop-daemon --start --quiet --make-pidfile --pidfile /tmp/custom_xvfb_99.pid \
-         --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1024x768x24
+         --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x768x24
 
     sudo apt-get update -qq
     sudo apt-get install python-pip python-virtualenv
