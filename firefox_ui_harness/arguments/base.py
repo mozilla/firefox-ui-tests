ce05996b836d5aef75ff7440e0d5b560b6126490
Henrik Skupin <mail@hskupin.info>
2015-09-02 13:04:15 +0200

    Bug 1198889 - Upgrade to marionette-client 0.19 and marionette-driver 0.13. r=ahal
    (cherry picked from commit cb162fadffb4b300a5a3a2e11f1def06c3117724)

---
+++ firefox_ui_harness/arguments/base.py
@@ -0,0 +1,43 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import firefox_puppeteer
+import firefox_ui_tests
+
+from marionette import BaseMarionetteArguments
+
+
+class FirefoxUIBaseArguments(object):
+    name = 'Firefox UI Tests'
+    args = [
+        [['--installer'], {
+            'help': 'Installer of a Gecko application to use for running the tests'
+        }],
+        [['--workspace'], {
+            'dest': 'workspace_path',
+            'help': 'Path to use for all temporary data. Defaults to TEMP.'
+        }]
+    ]
+
+    def parse_args_handler(self, args):
+        # Bug 1142064 - We cannot easily extent options because registered handlers
+        # are called at the end in BaseMarionetteArguments.verify_usage(). As result it
+        # will abort due to no binary specified. Once the bug is fixed we can move
+        # the whole block to verify_usage_handler().
+        if args.installer:
+            if args.binary:
+                raise ValueError('Options --binary and --installer are mutually exclusive.')
+
+            args.binary = 'FAKED_VALUE'
+
+        # If no tests are specified fall back to puppeteer unit and all firefox ui tests
+        args.tests = args.tests or [firefox_puppeteer.manifest, firefox_ui_tests.manifest_all]
+
+
+class FirefoxUIArguments(BaseMarionetteArguments):
+
+    def __init__(self, **kwargs):
+        BaseMarionetteArguments.__init__(self, **kwargs)
+
+        self.register_argument_container(FirefoxUIBaseArguments())
--- firefox_ui_harness/runtests.py
+++ firefox_ui_harness/runtests.py
@@ -10,9 +10,9 @@ import mozinstall
 
 from mozlog import structured
 
-from firefox_ui_harness.options import (FirefoxUIOptions,
-                                        UpdateOptions,
-                                        )
+from firefox_ui_harness.arguments import (FirefoxUIArguments,
+                                          UpdateArguments,
+                                          )
 
 from firefox_ui_harness.runners import (FirefoxUITestRunner,
                                         UpdateTestRunner,
@@ -57,20 +57,18 @@ def startTestRunner(runner_class, options, tests):
     return runner
 
 
-def cli(runner_class=FirefoxUITestRunner, parser_class=FirefoxUIOptions):
-    parser = parser_class(usage='%prog [options] test_file_or_dir <test_file_or_dir> ...')
+def cli(runner_class=FirefoxUITestRunner, parser_class=FirefoxUIArguments):
+    parser = parser_class(usage='%(prog)s [options] test_file_or_dir <test_file_or_dir> ...')
     structured.commandline.add_logging_group(parser)
-    options, tests = parser.parse_args()
-
-    parser.verify_usage(options, tests)
+    args = parser.parse_args()
+    parser.verify_usage(args)
 
     logger = structured.commandline.setup_logging(
-        options.logger_name, options, {'mach': sys.stdout})
-
-    options.logger = logger
+        args.logger_name, args, {'mach': sys.stdout})
+    args.logger = logger
 
     try:
-        runner = startTestRunner(runner_class, options, tests)
+        runner = startTestRunner(runner_class, args, args.tests)
         if runner.failed > 0:
             sys.exit(10)
 
@@ -81,7 +79,7 @@ def cli(runner_class=FirefoxUITestRunner, parser_class=FirefoxUIOptions):
 
 
 def cli_update():
-    cli(runner_class=UpdateTestRunner, parser_class=UpdateOptions)
+    cli(runner_class=UpdateTestRunner, parser_class=UpdateArguments)
 
 
 if __name__ == '__main__':
