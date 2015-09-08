973aaa59156d45894ed6c4cde3684b746f6c3d62
Armen Zambrano <armenzg@users.noreply.github.com>
2015-09-08 15:54:37 -0400

    Merge pull request #233 from armenzg/origin/python_script
    Bug 1201668 - Allow running Firefox UI update tests through a python wrapper script. r=whimboo
    (cherry picked from commit 310ff2d40229aac20a346a8597dda42dd7f225be)

--- firefox_ui_harness/__init__.py
+++ firefox_ui_harness/__init__.py
@@ -2,9 +2,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from runtests import (cli,
-                      cli_update,
-                      )
+from cli_update import cli_update
+from runtests import cli
 
 from testcases import (FirefoxTestCase,
                        UpdateTestCase,
