117f3c16c440fbf1e99325c8471b83cfee5c6eea
Axel Hecht <l10n@mozilla.com>
2015-09-11 22:23:50 +0200

    Bug 1204074 - more_info_button getter breaks if label isn't ascii. r=whimboo
    (cherry picked from commit 6e055a4727bcef4385f59b0e623a5944fc4fd593)

--- firefox_puppeteer/ui/toolbars.py
+++ firefox_puppeteer/ui/toolbars.py
@@ -583,7 +583,7 @@ class IdentityPopupSecurityView(IdentityPopupView):
         """
         label = self.window.get_entity('identity.moreInfoLinkText2')
 
-        return self.element.find_element(By.CSS_SELECTOR, 'button[label="{}"]'.format(label))
+        return self.element.find_element(By.CSS_SELECTOR, u'button[label="{}"]'.format(label))
 
     @property
     def owner(self):
