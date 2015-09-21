cee97b3c71b2534945a9f3aaf5f518229c5205e6
Henrik Skupin <mail@hskupin.info>
2015-09-21 22:20:24 +0200

    Bug 1204574 - Fix for missing changesets of merge commit

--- firefox_ui_harness/functional.py
+++
@@ -1,4 +0,0 @@
-from firefox_ui_harness import cli
-
-if __name__ == '__main__':
-    cli()
--- firefox_ui_harness/update.py
+++
@@ -1,4 +0,0 @@
-from firefox_ui_harness import cli_update
-
-if __name__ == '__main__':
-    cli_update()
