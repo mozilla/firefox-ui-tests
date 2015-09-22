9005ad748b126a52fb915919db5a72574a5bc4ac
Henrik Skupin <mail@hskupin.info>
2015-09-22 21:31:30 +0200

    Bug 1207284 - Make Travis CI specific settings for mozilla-beta. r=automatedtester

--- .travis/before_script.sh
+++ .travis/before_script.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 set -ev
 
-if [ "$TRAVIS_BRANCH" == "master" ]; then
-    OPTIONS="--type daily --branch mozilla-central"
-elif [ "$TRAVIS_BRANCH" == "mozilla-aurora" ]; then
-    OPTIONS="--type daily --branch mozilla-aurora"
-elif [ "$TRAVIS_BRANCH" == "mozilla-beta" ]; then
-    OPTIONS="--type release --version latest-beta"
-elif [ "$TRAVIS_BRANCH" == "mozilla-release" ]; then
-    OPTIONS="--type release --version latest"
-fi
-
-mozdownload $OPTIONS
+mozdownload --type tinderbox --branch mozilla-beta
