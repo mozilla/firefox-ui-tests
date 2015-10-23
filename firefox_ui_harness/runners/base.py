3502bc77f89234b33cb92ffbf91e812329a490fb
Maja Frydrychowicz <mjzffr@gmail.com>
2015-10-23 16:33:41 -0400

    Bug 1223543 - Make use of new MarionetteHarness, and bump to marionette-client 2.0.0 and marionette-driver 1.1.1. r=whimboo
    This involves bumping marionette versions to
    marionette-client 2.0.0 and marionette-driver 1.1.1
    
    (cherry picked from commit 32be49d74e1d10c6bf087235b1d6753c1b840bc4)

--- firefox_ui_harness/runners/base.py
+++ firefox_ui_harness/runners/base.py
@@ -11,63 +11,13 @@ import firefox_ui_tests
 from ..testcases import FirefoxTestCase
 
 
-DEFAULT_PREFS = {
-    'app.update.auto': False,
-    'app.update.enabled': False,
-    'browser.dom.window.dump.enabled': True,
-    # Bug 1145668 - Has to be reverted to about:blank once Marionette
-    # can correctly handle error pages
-    'browser.newtab.url': 'about:newtab',
-    'browser.newtabpage.enabled': False,
-    'browser.reader.detectedFirstArticle': True,
-    'browser.safebrowsing.enabled': False,
-    'browser.safebrowsing.malware.enabled': False,
-    'browser.search.update': False,
-    'browser.sessionstore.resume_from_crash': False,
-    'browser.shell.checkDefaultBrowser': False,
-    'browser.startup.page': 0,
-    'browser.tabs.animate': False,
-    'browser.tabs.warnOnClose': False,
-    'browser.tabs.warnOnOpen': False,
-    'browser.uitour.enabled': False,
-    'browser.warnOnQuit': False,
-    'dom.ipc.reportProcessHangs': False,
-    'dom.report_all_js_exceptions': True,
-    'extensions.enabledScopes': 5,
-    'extensions.autoDisableScopes': 10,
-    'extensions.getAddons.cache.enabled': False,
-    'extensions.installDistroAddons': False,
-    'extensions.logging.enabled': True,
-    'extensions.showMismatchUI': False,
-    'extensions.update.enabled': False,
-    'extensions.update.notifyUser': False,
-    'focusmanager.testmode': True,
-    'geo.provider.testing': True,
-    'javascript.options.showInConsole': True,
-    'marionette.logging': False,
-    'security.notification_enable_delay': 0,
-    'signon.rememberSignons': False,
-    'startup.homepage_welcome_url': 'about:blank',
-    'toolkit.startup.max_resumed_crashes': -1,
-    'toolkit.telemetry.enabled': False,
-}
-
-
 class FirefoxUITestRunner(BaseMarionetteTestRunner):
     def __init__(self, **kwargs):
         BaseMarionetteTestRunner.__init__(self, **kwargs)
-
+        # select the appropriate GeckoInstance
+        self.app = 'fxdesktop'
         if not self.server_root:
             self.server_root = firefox_ui_tests.resources
-
-        # Bug 1146847: This needs a refactoring given that our default
-        # preferences are not coming from the command line
-        self.prefs.update(DEFAULT_PREFS)
-
-        if not kwargs.get('e10s'):
-            self.prefs.update({'browser.tabs.remote.autostart': False})
-
-        self.workspace = kwargs.get('workspace')
 
         self.test_handlers = [FirefoxTestCase]
 
@@ -78,11 +28,3 @@ class FirefoxUITestRunner(BaseMarionetteTestRunner):
             return binary[:end_index]
         else:
             return os.path.dirname(binary)
-
-    def run_tests(self, tests):
-        # Ensure Marionette is always reset before starting tests
-        # This might need support in Marionette base
-        self.marionette = None
-        self.tests = []
-
-        BaseMarionetteTestRunner.run_tests(self, tests)
