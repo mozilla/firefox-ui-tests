1bc4bfd17eb3c9ba62ea819d5f310431d2a7a307
Barbara Miller <galgeek@me.com>
2015-11-09 21:27:03 -0800

    Bug 1204093 Improve Travis configs for multi locale support. r=whimboo
    (cherry picked from commit 27544cb1945abd4dbaa15d52015f45807210cd70)
    (cherry picked from commit b1e1990d16bbf823b981145039bc3f8e22c95a2e)

--- .travis/before_script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -ev
-
-if [[ $LOCALE = 'en-US' ]]
-then
-    mozdownload --type candidate --branch mozilla-release
-else
-    mozdownload --type candidate --branch mozilla-release --locale ru
-fi
