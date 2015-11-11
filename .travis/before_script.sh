f6f503985a4305dfb3c2b6e778467012fd864ba4
Barbara Miller <galgeek@me.com>
2015-11-10 16:43:05 -0800

    Bug 1204093 - Add a non-ascii locale to Travis CI testing matrix. r=whimboo
    (cherry picked from commit 57d76af6a12333a50d9393145ca8bae237f4680e)

--- .travis/before_script.sh
+++ .travis/before_script.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 set -ev
 
-mozdownload --type tinderbox --branch mozilla-beta
+if [[ $LOCALE = 'en-US' ]]
+then
+    mozdownload --type daily --branch mozilla-aurora
+else
+    mozdownload --type daily --branch mozilla-aurora --locale ru
+fi
